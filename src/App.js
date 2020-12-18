a671b103337cefe5d51ee8a682302903b32a83a6
Selim Yasser <selimabdelmonsef@gmail.com>
2020-12-18 03:40:43 +0200

    feat(new route): route to starredgithubpage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {landingPage} from './components/landing-page/landing-page';
+import { landingPage } from './components/landing-page/landing-page';
+import starredGithubPage from './components/starred-github-page/starred-github-page';
 
 
 export default class App extends React.Component {
   render() {
     return (
- 
+
       <Router>
 
         <Switch>
-<Route path = "/" exact component = {landingPage}/>
+          <Route path="/" exact component={landingPage} />
+          <Route path="/starredgithubpage" component={starredGithubPage} />
         </Switch>
       </Router >
     )
   }
 }
+ 
