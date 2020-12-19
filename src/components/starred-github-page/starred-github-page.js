cd0a5f4c877366b3207799b898fd781cb6c6b9f3
Selim Yasser <selimabdelmonsef@gmail.com>
2020-12-19 14:46:56 +0200

    feat(github page): github page design structure

--- src/components/starred-github-page/starred-github-page.js
+++ src/components/starred-github-page/starred-github-page.js
@@ -1,32 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { _GetRepositoryName } from '../../redux-action/repo-action'
+import styles from './starred-github-page.module.css'
+import Avatar from 'react-avatar';
+
 // import {}
 
 class starredGithubPage extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            name: ""
-        }
-    }
-
-    componentDidUpdate() {
-        console.log(11, this.props)
     }
 
     componentDidMount() {
         this.props.GetRepositoryName()
-
-        // console.log(12665432, this.props)
-
     }
 
     render() {
         return (
             <div>
-                SELIM 
+                <h1 className={styles.title}>
+                    The most starred Github repos created in the last 30 days
+                </h1>
+
+
+                {this.props?.data?.map((element, index) => {
+                    return (
+                        <div className={styles.starred_github_page}>
+                            <div className={styles.avatar_username}>
+                                <Avatar size="100" round={true} src={element?.avatar_url} />
+                                <p className={styles.username}>{element.username}</p>
+                            </div>
+                            <p className={styles.desc_name}>{index + 1}* {element?.name}</p>
+                            <div className={styles.desc_star}>
+                                <div>
+                                    <span>&#11088;</span>
+                                    <p className={styles.count}>{element?.stargazers_count}</p>
+                                </div>
+                                <p className={styles.description_base}>{element?.description}</p>
+                            </div>
+                            <p className={styles.issues_count}>Number of issues: {element?.open_issues_count}</p>
+
+                        </div>
+                    )
+
+                })}
+
             </div>
         )
     }
