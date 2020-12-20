d0148755ae889a659aad23ca60bc380775c073d2
Selim Yasser <selimabdelmonsef@gmail.com>
2020-12-20 23:23:25 +0200

    style(github-starred-page): pagination to bottom and containers margin

--- src/components/starred-github-page/starred-github-page.js
+++ src/components/starred-github-page/starred-github-page.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { _GetRepositoryName } from '../../redux-action/repo-action'
-import styles from './starred-github-page.module.css'
+import { _GetRepositoryName } from '../../redux-action/repo-action';
+import styles from './starred-github-page.module.css';
 import Avatar from 'react-avatar';
+import PaginationFooter from '../shared/pagination/pagination'
+import { paginationPage } from '../shared/pagination/pagination';
+// import {current_page} from '../shared/pagination/pagination'
 
-// import {}
 
 class starredGithubPage extends React.Component {
 
@@ -12,41 +14,50 @@ class starredGithubPage extends React.Component {
         super(props);
     }
 
+
+
     componentDidMount() {
         this.props.GetRepositoryName()
     }
 
     render() {
+
         return (
             <div>
+
                 <h1 className={styles.title}>
                     The most starred Github repos created in the last 30 days
                 </h1>
-
-
                 {this.props?.data?.map((element, index) => {
-                    return (
-                        <div className={styles.starred_github_page}>
-                            <div className={styles.avatar_username}>
-                                <Avatar size="100" round={true} src={element?.avatar_url} />
-                                <p className={styles.username}>{element.username}</p>
-                            </div>
-                            <p className={styles.desc_name}>{index + 1}* {element?.name}</p>
-                            <div className={styles.desc_star}>
-                                <div>
-                                    <span>&#11088;</span>
-                                    <p className={styles.count}>{element?.stargazers_count}</p>
-                                </div>
-                                <p className={styles.description_base}>{element?.description}</p>
-                            </div>
-                            <p className={styles.issues_count}>Number of issues: {element?.open_issues_count}</p>
 
+                    return (
+
+                        <div>
+                            <div className={styles.starred_github_page}>
+                                <div className={styles.avatar_username}>
+                                    <Avatar size="100" round={true} src={element?.avatar_url} />
+                                    <p className={styles.username}>{element.username}</p>
+                                </div>
+                                <p className={styles.desc_name}>{index + 1}* {element?.name}</p>
+                                <div className={styles.desc_star}>
+                                    <div>
+                                        <span>&#11088;</span>
+                                        <p className={styles.count}>{element?.stargazers_count}</p>
+                                    </div>
+                                    <p className={styles.description_base}>{element?.description}</p>
+                                </div>
+                                <p className={styles.issues_count}>Number of issues: {element?.open_issues_count}</p>
+
+                            </div>
                         </div>
                     );
 
                 })}
 
+                <PaginationFooter></PaginationFooter>
             </div>
+
+
         )
     }
 }
